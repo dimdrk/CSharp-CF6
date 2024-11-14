52581e69b7ccb53231341a55eae1110769a87376
DimDrk <dimdrk@gmail.com>
2024-11-14 09:58:42 +0200

    initial commit

---
+++ IgnoreNegatives/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IgnoreNegatives
+{
+    /// <summary>
+    /// Counts only the negatives.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int negativesCount = 0;
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Please insert a number (integer)");
+                    num = int.Parse(Console.ReadLine()!);
+                    if (num > 0) continue;
+                    negativesCount++;
+                    if (num == 0) break;
+                } while (true);
+            } catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }            
+        }
+    }
+}
