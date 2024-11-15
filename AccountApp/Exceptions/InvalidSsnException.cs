ef896933e887eb46208c354ad3ea63203912f2ab
DimDrk <dimdrk@gmail.com>
2024-11-15 14:54:07 +0200

    AccountApp created

---
+++ AccountApp/Exceptions/InvalidSsnException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountApp.Exceptions
+{
+    internal class InvalidSsnException : Exception
+    {
+        public InvalidSsnException(string ssn) 
+            : base("SSN " + ssn + " is not valid.")
+        {
+
+        }
+    }
+}
