924b73009c747e78f44dba7ed71506f9e1339817
DimDrk <dimdrk@gmail.com>
2024-11-15 13:31:36 +0200

    ObjectOrientedApp created

---
+++ OOApp/Model/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOApp.Model
+{
+    /// <summary>
+    /// POCO class
+    /// </summary>
+    internal class Teacher
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+
+        public Teacher()
+        {
+            
+        }
+
+        public Teacher(int id, string? firstname, string? lastname)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
---
+++ OOApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using OOApp.Model;
+
+namespace OOApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher alice = new Teacher();
+            Teacher bob = new();                       // >= C# 9
+            var costas = new Teacher();                // syntactic sugar
+
+            Teacher anna = new Teacher()                        // Object Initializer - Needs default constractor + setters or 'init'
+            {
+                Id = 1,
+                Firstname = "Anna",
+                Lastname = "Giannoutsou"
+            };
+
+            Teacher andreas = new Teacher( 2, "Andreas", "Androutsos");     //Overload constractor - Needs all fields
+        }
+    }
+}
