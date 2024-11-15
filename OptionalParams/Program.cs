69821c4f329206480376df711a54b74ed7108492
DimDrk <dimdrk@gmail.com>
2024-11-15 12:34:15 +0200

    OptionalParams created

---
+++ OptionalParams/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Timers;
+
+namespace OptionalParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = GetDateTime();
+            Console.WriteLine($"{dateTime:D}");
+
+            DateTime dateTime2 = GetDateTime();
+            Console.WriteLine($"{dateTime:F}");
+        }
+
+        public static DateTime GetDateTime(int year=2024, int month=11, int day=1, int min=0, int sec=0, int milli=0)
+        {
+            return new DateTime(year, month, day, min, sec, milli, DateTimeKind.Utc);
+        }
+    }
+}
