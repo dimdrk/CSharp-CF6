b1aab3b0ff38ac3e0141ba9317138633a65cbc83
DimDrk <dimdrk@gmail.com>
2024-11-16 19:55:40 +0200

    Fraction created

--- CopyingApp/Model/Student.cs
+++ CopyingApp/Model/Student.cs
@@ -8,5 +8,41 @@ namespace CopyingApp.Model
 {
     internal class Student
     {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public Address? Address { get; set; }
+
+        public Student() { }
+
+        public Student(int id, string? firstname, string? lastname, Address? address)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Address = address;
+        }
+
+        public Student GetRef()
+        {
+            return this;
+        }
+
+        public Student GetShallowCopy()
+        {
+            return (Student)MemberwiseClone();
+        }
+
+        public Student GetDeepCopy()
+        {
+            Student student = (Student)MemberwiseClone();
+            student.Address = new Address()
+            {
+                StreetName = Address!.StreetName,
+                StreetNo = Address!.StreetNo,
+                ZipCode = Address!.ZipCode
+            };
+            return student;
+        }
     }
 }
