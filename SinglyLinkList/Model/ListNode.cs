90e80a2c549378623e4542a537514e723dfc5d19
DimDrk <dimdrk@gmail.com>
2024-11-18 22:53:14 +0200

    SinglyLinkedList

---
+++ SinglyLinkList/Model/ListNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkList.Model
+{
+    internal class ListNode<T>
+    {
+        public T? Value { get; set; }
+        public ListNode<T>? Next { get; set; }
+    }
+}
---
+++ SinglyLinkList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SinglyLinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
