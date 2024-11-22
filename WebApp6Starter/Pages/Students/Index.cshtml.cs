823aecb38d0778903b79329925f2438c3628275d
DimDrk <dimdrk@gmail.com>
2024-11-22 20:04:42 +0200

    updates

--- WebApp6Starter/Pages/Students/Index.cshtml.cs
+++ WebApp6Starter/Pages/Students/Index.cshtml.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp6Starter.DTO;
 
 namespace WebApp6Starter.Pages.Students
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]  // Two-way binding
+        public InsertStudentDTO InsertStudentDTO { get; set; } = new();
+
         public void OnGet()
         {
         }
