ee08bdfcb53d186267e07ab27620bd42e8ac1861
DimDrk <dimdrk@gmail.com>
2024-11-22 21:48:27 +0200

    updates

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿USE Students6DB;
+GO
+
+CREATE TABLE Students (
+	Id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Firstname NVARCHAR(50) NOT NULL,
+	Lastname NVARCHAR(50) NOT NULL
+)
+GO
+
+INSERT INTO Students (Firstname, Lastname)
+VALUES
+	('Νίκος','Παπαδόπουλος'),
+	('Πέτρος','Αλεξόπουλος'),
+	('Ελένη','Αλεξανδρή');
---
+++ WebApp6Starter/Pages/Students/Success.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp6Starter.Pages.Students
+{
+    public class SuccessModel : PageModel
+    {
+        public string? Message { get; set; }
+        public void OnGet()
+        {
+            Message = TempData["Message"] as string ?? "Operation completed successfully";
+        }
+    }
+}
