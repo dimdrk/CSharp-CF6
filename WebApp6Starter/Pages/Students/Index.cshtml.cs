9593dd7770fb835942685a580f590ecce24db68d
DimDrk <dimdrk@gmail.com>
2024-11-22 19:59:17 +0200

    updates

---
+++ WebApp6Starter/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp6Starter.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
