1db3f62b281837d31ca9a94c24969bb00d49b614
DimDrk <dimdrk@gmail.com>
2024-11-24 17:32:22 +0200

    StudentUpdateDTO

---
+++ WebStarter6DBApp/DTO/StudentUpdateDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStarter6DBApp.DTO
+{
+    public class StudentUpdateDTO : BaseDTO
+    {
+        [Required(ErrorMessage = "Firstname is required.")]
+        [MinLength(1, ErrorMessage = "Firstname must be at least 1 character long.")]
+        public string? Firstname { get; set; }
+        [Required(ErrorMessage = "Lastname is required.")]
+        [MinLength(1, ErrorMessage = "Lastname must be at least 1 character long.")]
+        public string? Lastname { get; set; }
+
+        public StudentUpdateDTO() { }
+
+        public StudentUpdateDTO(int id, string? firstname, string? lastname)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
