6c447ca54fde3f36a1e8e180ec73e25fba488ae9
DimDrk <dimdrk@gmail.com>
2024-11-24 17:46:51 +0200

    MapperConfig created

---
+++ WebStarter6DBApp/Configuration/MapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using WebStarter6DBApp.DTO;
+using WebStarter6DBApp.Models;
+
+namespace WebStarter6DBApp.Configuration
+{
+    public class MapperConfig : Profile
+    {
+
+        public MapperConfig()
+        {
+            CreateMap<StudentInsertDTO, Student>().ReverseMap();
+            CreateMap<StudentUpdateDTO, Student>().ReverseMap();
+            CreateMap<StudentReadOnlyDTO, Student>().ReverseMap();
+        }
+    }
+}
