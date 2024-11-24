e39e2850511f05ecefa4c1975873515d0c2ae5fe
DimDrk <dimdrk@gmail.com>
2024-11-24 15:12:55 +0200

    IstudentDAO created

---
+++ WebStarter6DBApp/DAO/IStudentDAO.cs
@@ -0,0 +1,13 @@
+﻿using WebStarter6DBApp.Models;
+
+namespace WebStarter6DBApp.DAO
+{
+    public interface IStudentDAO
+    {
+        Student? Insert(Student student);
+        void Update(Student student);
+        void Delete(int Id);
+        Student? GetById(int Id);
+        List<Student> GetAll();
+    }
+}
