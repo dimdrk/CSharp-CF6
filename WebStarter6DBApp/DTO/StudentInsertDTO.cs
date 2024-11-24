ee8a8cabb94370b5786ec60e9f78cbb8eba04ca2
DimDrk <dimdrk@gmail.com>
2024-11-24 17:29:15 +0200

    StudentInsertDTO created

---
+++ WebStarter6DBApp/DTO/StudentInsertDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStarter6DBApp.DTO
+{
+    public class StudentInsertDTO
+    {
+        [Required(ErrorMessage = "Firstname is required.")]
+        [MinLength(1, ErrorMessage = "Firstname must be at least 1 character long.")]
+        public string? Firstname { get; set; }
+        [Required(ErrorMessage = "Lastname is required.")]
+        [MinLength(1, ErrorMessage = "Lastname must be at least 1 character long.")]
+        public string? Lastname { get; set; }
+
+        public StudentInsertDTO() { }
+
+        public StudentInsertDTO(string? firstname, string? lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
