5ca77228a6817751b4d4ead724a233c522448505
DimDrk <dimdrk@gmail.com>
2024-11-26 22:46:16 +0200

    updates

--- WebStarter6DBApp/Pages/Students/Create.cshtml.cs
+++ WebStarter6DBApp/Pages/Students/Create.cshtml.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebStarter6DBApp.DTO;
+using WebStarter6DBApp.Models;
+using WebStarter6DBApp.Services;
 
 namespace WebStarter6DBApp.Pages.Students
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public StudentInsertDTO StudentInsertDTO { get; set; } = new();
+
+        public List<Error> ErrorArray { get; set; } = [];
+
+        private readonly IStudentService _studentService;
+
+        public CreateModel(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+
+            try
+            {
+                StudentReadOnlyDTO? studentReadOnlyDTO = _studentService.InsertStudent(StudentInsertDTO);
+                Response.Redirect("/Students/getall");
+            }
+            catch (Exception ex)
+            {
+                ErrorArray.Add(new Error("", ex.Message, ""));
+                return;
+            }
         }
     }
 }
